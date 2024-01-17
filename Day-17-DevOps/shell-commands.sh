c58ad388e1aed41f66c9fab1e7cb7f3d8f0bde61
Sai Prajoth <87938462+SaiPrajoth@users.noreply.github.com>
2024-01-17 15:22:46 +0530

    Create shell-commands.sh

---
+++ Day-17-DevOps/shell-commands.sh
@@ -0,0 +1,16 @@
+pwd # to know where you are now in your machine or the environment, present working directory.
+ls # to list the contents of the present working directory.
+ls -ltr # to list the contents of present working directory, with the time stamps, file nature and other properties of the contents.
+cd <path> # change directory to a path, may be a directory or to a folder.
+
+touch abc.txt # creates a file called abc.txt
+vi def.txt # creates the file and will let you start writing into the file. 
+mkdir directory_name # creates a directory
+rm directory_name # to remove a directory
+rm -r file_name # to remove a file
+
+free -g # to check the memory of the laptop or the environment you are in
+nproc # to check the number of CPUs working for the machine.
+df -h # to check the disk size of the machine or the environment you are in
+
+top # it provides all the information for memory, cpus used and disk size.
